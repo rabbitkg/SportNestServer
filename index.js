d3229fbd0626d2015c4dd208c14c2c0bbae526b4
rabbitkg <rabbitkgbd@gmail.com>
2026-05-19 15:00:48 +0600

    create post API for add facility.

--- index.js
+++ index.js
@@ -3,13 +3,17 @@
 
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
 
 
 const client = new MongoClient(uri, {
@@ -22,15 +26,26 @@ const client = new MongoClient(uri, {
 
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
+  try {    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const db = client.db("sportNest")
+    const facilityCollection = db.collection("facility")
+
+    app.post('/facility', async (req, res) => {
+      const facilityData = req.body
+      console.log(facilityData)
+      const result = await facilityCollection.insertOne(facilityData)
+
+      res.json(result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
